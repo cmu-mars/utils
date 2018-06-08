f27d98d653bcd1762745707bdf8a07cca0e42616
Bradley Schmerl <schmerl@github.com>
2018-06-08 13:17:42 -0400

    Join instructions now takes config-specific data

--- join-instructions.py
+++ join-instructions.py
@@ -25,8 +25,8 @@ def load_instructions(src, target, dir):
 def process_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output_db", type=str, help='The file to output the instruction DB to')
+    parser.add_argument('-c', "--config_data", action="append", nargs=2, metavar=('config','dir'), help='For config the data is in dir',required=True)
     parser.add_argument("num_waypoints", type=int, help='The number of the highest waypoint')
-    parser.add_argument("dir", type=str, help="The directory containing instructions and paths")
 
     args = parser.parse_args()
 
@@ -34,11 +34,10 @@ def process_args():
         args.output_db = 'instructions-all.json'
 
     args.output_db = os.path.expandvars(args.output_db)
-    args.dir = os.path.expandvars(args.dir)
-    if not os.path.isdir(args.dir):
-        print("The directory '%s' does not exist" %args.dir)
-        sys.exit()
-
+    for d in args.config_data:
+        d[1] = os.path.expandvars(d[1])
+        if not os.path.isdir(d[1]):
+            print("The directory '%s' does not exist" %d[1])
     return args
 
 
@@ -47,21 +46,27 @@ db = {}
 
 wrong = 0
 right = 0
-for i in range(1, args.num_waypoints+1):
-    for j in range(1, args.num_waypoints+1):
-        if i != j:
-            try:
-                path_info = load_path(i,j, args.dir)
-                instructions = load_instructions(i,j, args.dir)
-                path_info["instructions"] = instructions
-                db["l%s_to_l%s" %(i,j)] = path_info
-                right = right + 1
-            except IOError:
-                print("Warning: No path or instructions for l%s to l%s" %(i,j))
-                wrong = wrong + 1
-            except Exception:
-                wrong = wrong + 1
-                print("Warning: Something bad happened for l%s to l%s" %(i,j))
+for d in args.config_data:
+    for i in range(1, args.num_waypoints+1):
+        for j in range(1, args.num_waypoints+1):
+            if i != j:
+                try:
+                    path_info = load_path(i,j, d[1])
+                    instructions = load_instructions(i,j, d[1])
+                    path_info["instructions"] = instructions
+                    key = "l%s_to_l%s" %(i,j)
+                    if key in db.keys():
+                        db[key][d[0]] = path_info
+                    else:
+                        db[key] = {}
+                        db[key][d[0]] = path_info
+                    right = right + 1
+                except IOError:
+                    print("Warning: No path or instructions for l%s to l%s in %s" %(i,j,d[0]))
+                    wrong = wrong + 1
+                except Exception:
+                    wrong = wrong + 1
+                    print("Warning: Something bad happened for l%s to l%s in %s" %(i,j,d[0]))
 
 if wrong > 0:
     print("There were a total of %s filled in and %s missing entries " %(right,wrong))
